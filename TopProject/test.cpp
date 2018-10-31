9e5e09917d24667c96df8984aaa7626cc93c7582
kirkpartridge <kirk.partridge@gmail.com>
2018-10-30 20:02:24 -0600

    No Comment

--- TopProject/test.cpp
+++ TopProject/test.cpp
@@ -2,7 +2,6 @@
 #include "./Backend/include/StockModel.h"
 int main( int argc, char **argv )
 {
-    TransactionType test;
     StockModel model;
     model.GetTransactionsFromCSV("/Users/kirkpartridge/Desktop/Stocks.csv");    
 	return 1;
